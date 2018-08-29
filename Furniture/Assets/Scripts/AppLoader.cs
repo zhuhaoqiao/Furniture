102f1ee6876f14ee8019ffcc1e37467a24672c09
DESKTOP-S5L3H66\L <qiao@loonqin.co>
2018-08-29 15:17:21 +0800

    1.0.0.5 添加GameScenes

---
+++ Furniture/Assets/Scripts/AppLoader.cs
@@ -0,0 +1,20 @@
+﻿using QFramework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppLoader : MonoBehaviour {
+
+    [SerializeField] UICtrl mUICtrl;
+    private void Awake()
+    {
+        Log.I("Init[{0}]");
+    }
+
+    private void Start()
+    {
+        Input.multiTouchEnabled = false;
+        mUICtrl.gameObject.Show();
+        Destroy(gameObject);
+    }
+}
